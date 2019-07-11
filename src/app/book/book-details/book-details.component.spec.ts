aa8e9d6c33e2f781b596094aa624ae972bc5889f
mmatczak <marek.matczak@capgemini.com>
2019-07-11 12:17:34 +0200

    class test added

--- src/app/book/book-details/book-details.component.spec.ts
+++ src/app/book/book-details/book-details.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BookDetailsComponent } from './book-details.component';
 
-describe('BookDetailsComponent', () => {
-  let component: BookDetailsComponent;
-  let fixture: ComponentFixture<BookDetailsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BookDetailsComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+fdescribe('BookDetailsComponent', () => {
+  describe('(class)', () => {
+    it('emits an event upon book change', () => {
+      // given
+      const eventMock: any = {
+        preventDefault: jasmine.createSpy('preventDefault'),
+        target: {
+          querySelector(selector: string) {
+            return selector === 'input#author'
+              ? {value: 'updatedAuthor'} : {value: 'updatedTitle'};
+          }
+        }
+      };
+      const component = new BookDetailsComponent();
+      component.book = {
+        id: 0,
+        title: 'test',
+        author: 'test'
+      };
+      component.bookChange.subscribe(updatedBook => {
+        // then
+        expect(eventMock.preventDefault).toHaveBeenCalled();
+        expect(updatedBook).toBeDefined();
+        expect(updatedBook.title).toBe('updatedTitle');
+        expect(updatedBook.author).toBe('updatedAuthor');
+      });
+      // when
+      component.notifyOnBookChange(eventMock);
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // describe();
 });
